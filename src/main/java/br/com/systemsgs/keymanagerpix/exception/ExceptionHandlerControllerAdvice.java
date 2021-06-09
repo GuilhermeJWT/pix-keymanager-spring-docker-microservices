a415f7e4bedee4102216cf8cf67cfaf74204e6b6
GuilhermeJWT <guiromanno@gmail.com>
2021-06-09 18:53:05 -0300

    Adicionando - Exception Handler Advice

---
+++ src/main/java/br/com/systemsgs/keymanagerpix/exception/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,32 @@
+package br.com.systemsgs.keymanagerpix.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+	
+	@ExceptionHandler(GenerationExceptionClass.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public RestApiErrorsException tratamentoException(GenerationExceptionClass generationExceptionClass) {
+		String mensagemErro = generationExceptionClass.getLocalizedMessage();
+		return new RestApiErrorsException(mensagemErro);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public RestApiErrorsException exibeErrosParaCliente(MethodArgumentNotValidException methodArgumentNotValidException) {
+		List<String> errors = methodArgumentNotValidException.getBindingResult().getAllErrors().stream()
+				.map(erro -> erro.getDefaultMessage()).collect(Collectors.toList());
+
+		return new RestApiErrorsException(errors);
+
+	}
+
+}
---
+++ src/main/java/br/com/systemsgs/keymanagerpix/exception/GenerationExceptionClass.java
@@ -0,0 +1,12 @@
+package br.com.systemsgs.keymanagerpix.exception;
+
+public class GenerationExceptionClass extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public GenerationExceptionClass(String mensagem) {
+		super(mensagem);
+	}
+	
+
+}
