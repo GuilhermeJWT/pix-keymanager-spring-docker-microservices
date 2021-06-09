b9f163b190b57989f2ba8e3783c1ede131cc1186
GuilhermeJWT <guiromanno@gmail.com>
2021-06-09 18:31:13 -0300

    Setup Inicial - Pix Spring Microservices

---
+++ src/main/java/br/com/systemsgs/keymanagerpix/KeymanagerPixApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.keymanagerpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeymanagerPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeymanagerPixApplication.class, args);
+	}
+
+}
