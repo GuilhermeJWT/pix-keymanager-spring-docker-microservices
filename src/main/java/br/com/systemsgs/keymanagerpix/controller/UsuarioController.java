dedd5492079c52b015cee240f3037e2612623dba
GuilhermeJWT <guiromanno@gmail.com>
2021-06-11 10:36:37 -0300

    Consumindo o Serviço de recupra todos Clientes Itau

--- src/main/java/br/com/systemsgs/keymanagerpix/controller/UsuarioController.java
+++ src/main/java/br/com/systemsgs/keymanagerpix/controller/UsuarioController.java
@@ -4,12 +4,14 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.systemsgs.keymanagerpix.dto.ModelUsuarioDTO;
+import br.com.systemsgs.keymanagerpix.feign.ItauClient;
 import br.com.systemsgs.keymanagerpix.service.UsuarioService;
 
 @RestController
@@ -19,12 +21,20 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private ItauClient itauClient;
+	
 	@PostMapping(value = "/salvar")
 	@Transactional
 	public String salvaUsuarioTeste(@RequestBody @Valid ModelUsuarioDTO modelUsuarioDTO){
 	   ModelUsuarioDTO usuarioSalvoDTO =  usuarioService.salvaUsuario(modelUsuarioDTO);
 	   
 		return usuarioSalvoDTO.getChave();
+	}
+	
+	@GetMapping(value = "/pesquisaUsuariosItau")
+	public String retornaTodosClientesItau() {
+		return itauClient.retornaTodosClientesItau();
 	}
 	
 }
--- src/main/java/br/com/systemsgs/keymanagerpix/feign/ItauClient.java
+++ src/main/java/br/com/systemsgs/keymanagerpix/feign/ItauClient.java
@@ -1,9 +1,17 @@
 package br.com.systemsgs.keymanagerpix.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.systemsgs.keymanagerpix.dto.ModelUsuarioDTO;
 
 @FeignClient(name = "contas-clientes", url = "http://localhost:9091")
 public interface ItauClient {
 	
-
+	@GetMapping(value = "/api/v1/private/contas/todas")
+	String retornaTodosClientesItau();
+	
+	@GetMapping(value = "/api/v1/clientes/{clienteId}/contas")
+	ModelUsuarioDTO retornaClientePorIdItau();
+	
 }
